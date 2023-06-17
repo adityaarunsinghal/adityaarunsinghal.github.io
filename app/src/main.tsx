aaf15ce9484a75f32a8e47d2aadc6d0d7839c5fa
Aditya Singhal <35391599+adityaarunsinghal@users.noreply.github.com>
2023-06-17 11:30:44 -0700

    Combine old and new apps

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+// import './assets/css/react-app-index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('react-app') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
