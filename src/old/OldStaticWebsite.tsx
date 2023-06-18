45ece1298a4083609a04379b5a94f91e75a04062
Aditya Singhal <35391599+adityaarunsinghal@users.noreply.github.com>
2023-06-17 17:56:59 -0700

    Copy over CNAME automatically when deploying

---
+++ src/old/OldStaticWebsite.tsx
@@ -0,0 +1,11 @@
+export function OldStaticWebsite() {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <iframe
+        src='/old-static/index.html'
+        title='Aditya Singhal'
+        style={{ width: '100%', height: '100%', border: 'none' }}
+      ></iframe>
+    </div>
+  );
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-
-function OldStaticWebsite() {
-  return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <iframe
-        src='/old-static/index.html'
-        title='Aditya Singhal'
-        style={{ width: '100%', height: '100%', border: 'none' }}
-      ></iframe>
-    </div>
-  );
-}
+import { OldStaticWebsite } from './old/OldStaticWebsite.tsx';
 
 export default function Router() {
   return createBrowserRouter([
